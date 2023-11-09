00d736518ee7ea2377e0fc459108a97ea9361cdf
qwertyuslaxmax <147653359+qwertyuslaxmax@users.noreply.github.com>
2023-11-09 02:34:34 +0000

    hocus pocus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,76 @@
 <script lang="ts">
-    
-    let gridButtons:number[] = []
-    for(let i=0;i<64;i++){
-        gridButtons[i]=i
+    let gridButtons: number[] = [];
+
+    for (let i = 0; i < 64; i++) {
+        gridButtons[i] = i;
     }
+
+    let bombClicked = false;
+
     function getRandomNumbers() {
-        var randomNumbers:number[] = [];
-  
-        while (randomNumbers.length < 8) {
-            var randomNumber = Math.floor(Math.random() * 64); // Generates a random number between 0 and 63 (inclusive).
-    
+        let randomNumbers: number[] = [];
+
+        while (randomNumbers.length < 12) {
+            let randomNumber = Math.floor(Math.random() * 64);
+
             if (!randomNumbers.includes(randomNumber)) {
-            randomNumbers.push(randomNumber);
+                randomNumbers.push(randomNumber);
             }
         }
-        
 
-  return randomNumbers;
-  
-}
-var randomNumbers = getRandomNumbers();
-console.log(randomNumbers)
+        return randomNumbers;
+    }
+
+    let bombNumbers = getRandomNumbers();
+    console.log(bombNumbers);
+
+    let disabledButtons: number[] = [];
+
+    function handleClick(num: number) {
+        if (bombNumbers.includes(num)) {
+            bombClicked = true;
+            disabledButtons = bombNumbers.slice(); // Disable all bomb buttons
+        }
+    }
 </script>
-<div class="flex justify-center">
-<div class="grid grid-cols-8 gap-2 w-fit">
-    {#each gridButtons as num}
-    
-    {#if randomNumbers.includes(num)} 
-    <button class="text-red-600 bg-blue-500 w-16 h-16">
-        {num}
-    </button>
-    {:else}
-    
-    <button class="text-blue-600 bg-orange-500 w-16 h-16">
-        {num}
-    </button>
+
+<div class="flex justify-center align-items: center">
+    <div class="grid grid-cols-8 gap-2 w-fit">
+        {#each gridButtons as num}
+            {#if bombClicked}
+                <button
+                    class="text-blue-600 bg-orange-500 w-16 h-16"
+                    on:click={() => handleClick(num)}  
+                    disabled={disabledButtons.includes(num)}
+                >
+                </button>
+            {:else}
+                <button
+                    class="text-blue-600 bg-orange-500 w-16 h-16 focus:bg-blue-400 visited:bg-blue-300"
+                    on:click={() => handleClick(num)}
+                >
+                </button>
+            {/if}
+        {/each}
+    </div>
+</div>
+{#if bombClicked}
+        <p class="message-overlay">You Lost</p>
     {/if}
-    {/each}
-</div>
-</div>
 <style>
+    .message-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.7);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: white;
+        font-size: 1.5rem;
+        z-index: 999; /* Ensure it's on top of everything */
+    }
 
 </style>
